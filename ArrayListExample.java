b6d0c539341d26e0c0fa104c81b8d69416043723
Gaurav Keshri <gkeshri.keshri3@gmail.com>
2022-02-21 14:20:18 +0530

    initial commit

---
+++ ArrayListExample.java
@@ -0,0 +1,29 @@
+package com.citi;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<10;i++) {
+		al.add(i);
+		}
+		System.out.print("Before Replacement:");
+		for(Integer j:al) {
+			System.out.print(al.get(j)+" ");
+		}
+		
+		al.set(1, 2);
+		System.out.print("After Replacement:");
+		for(Integer j:al) {
+			System.out.print(al.get(j)+" ");
+		}
+	
+		
+		
+
+	}
+
+}
